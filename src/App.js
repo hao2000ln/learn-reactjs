159bf59b9744000c06d306ab8a4cc05baea89d59
HAOLN <haohanghai611@gmail.com>
2022-09-09 00:36:51 +0700

    edit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        Lương Ngọc Hảo
+        Haoln changes
       </header>
     </div>
   );
